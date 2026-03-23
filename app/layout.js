ef30995367859bec9316c783ab82f58c8ad216b0
Ryan <ryan@Ryans-MacBook-Air.local>
2026-03-23 11:46:01 -1000

    Add Google Tag Manager (GTM-KHJ57FX2)

--- app/layout.js
+++ app/layout.js
@@ -86,7 +86,11 @@ const jsonLd = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KHJ57FX2');` }} />
+      </head>
       <body>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KHJ57FX2" height="0" width="0" style={{ display: "none", visibility: "hidden" }} /></noscript>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
