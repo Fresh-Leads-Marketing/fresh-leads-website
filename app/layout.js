23bdeed74d870588636016b12f8b37bf84325953
Fresh-Leads-Marketing <ryan@freshleadsmarketing.com>
2026-03-31 19:45:35 -1000

    Add ClickCease click fraud protection tracking

--- app/layout.js
+++ app/layout.js
@@ -88,6 +88,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KHJ57FX2');` }} />
+        {/* ClickCease.com click fraud protection */}
+        <script dangerouslySetInnerHTML={{ __html: `var script = document.createElement('script');script.async = true;script.type = 'text/javascript';var target = 'https://www.clickcease.com/monitor/stat.js';script.src = target;var elem = document.head;elem.appendChild(script);` }} />
+        <noscript><a href="https://www.clickcease.com" rel="nofollow"><img src="https://monitor.clickcease.com" alt="ClickCease" /></a></noscript>
       </head>
       <body>
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KHJ57FX2" height="0" width="0" style={{ display: "none", visibility: "hidden" }} /></noscript>
